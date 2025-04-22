f3b74fdff8cb58990835168f13b9f52174d24e0a
Тимофей <kalyakta@gmail.com>
2025-04-22 20:48:33 +0700

    render

--- app.py
+++ app.py
@@ -1,11 +1,67 @@
 from flask import Flask, request
 import logging
 import json
-from geo import get_country, get_distance, get_coordinates
+import requests
+
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO, filename='app.log', format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+from math import sin, cos, sqrt, atan2, radians
+
+
+def get_distance(p1, p2):
+    R = 6373.0
+
+    lon1 = radians(p1[0])
+    lat1 = radians(p1[1])
+    lon2 = radians(p2[0])
+    lat2 = radians(p2[1])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance
+
+
+def get_country(city):
+    url = "https://geocode-maps.yandex.ru/1.x/"
+
+    params = {
+        'geocode': city,
+        'format': 'json',
+        'apikey': "40d1649f-0493-4b70-98ba-98533de7710b"
+    }
+
+    response = requests.get(url, params)
+    json = response.json()
+
+    return \
+        json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
+            'GeocoderMetaData'][
+            'AddressDetails']['Country']['CountryName']
+
+
+def get_coordinates(city):
+    url = "https://geocode-maps.yandex.ru/1.x/"
+
+    params = {
+        'geocode': city,
+        'format': 'json',
+        'apikey': "40d1649f-0493-4b70-98ba-98533de7710b"
+    }
+
+    response = requests.get(url, params)
+    json = response.json()
+    point_str = json['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+    point_array = [float(x) for x in point_str.split(' ')]
+
+    return point_array
 
 
 @app.route('/post', methods=['POST'])
@@ -27,6 +83,15 @@ def main():
     return json.dumps(response)
 
 
+def get_cities(req):
+    cities = []
+    for entity in req['request']['nlu']['entities']:
+        if entity['type'] == 'YANDEX.GEO':
+            if 'city' in entity['value'].keys():
+                cities.append(entity['value']['city'])
+    return cities
+
+
 def handle_dialog(res, req):
     user_id = req['session']['user_id']
 
@@ -46,15 +111,6 @@ def handle_dialog(res, req):
             round(distance)) + ' км.'
     else:
         res['response']['text'] = 'Слишком много городов!'
-
-
-def get_cities(req):
-    cities = []
-    for entity in req['request']['nlu']['entities']:
-        if entity['type'] == 'YANDEX.GEO':
-            if 'city' in entity['value'].keys():
-                cities.append(entity['value']['city'])
-    return cities
 
 
 if __name__ == '__main__':
